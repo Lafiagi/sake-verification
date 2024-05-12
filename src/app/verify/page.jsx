b8dc5beeb163ac2abd3117ab60b6414bc9752122
lafiagi <skillfullmallam@gmail.com>
2024-05-12 08:37:04 +0100

    Added the message

---
+++ src/app/verify/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Box, Text } from "@chakra-ui/react";
+
+const Verify = () => {
+  return (
+    <Box>
+      <Text fontSize={20} mt={50} textAlign={"center"} fontWeight={"bold"} color={"#f22"}>
+        Please register before proceeding to the verification process
+      </Text>
+    </Box>
+  );
+};
+export default Verify;
